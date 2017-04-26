0bf5fde24b28a51a7d2d7586768eaa63039ce40f
David Martinez <davidmgilo@gmail.com>
2017-04-26 19:42:43 +0200

    avoiding some errors I

--- resources/assets/js/animation.js
+++ resources/assets/js/animation.js
@@ -1,8 +1,16 @@
 function init() {
     //Draw first frame
     var c = document.getElementById("animation");
-    var ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(95,50,40,0,2*Math.PI);
-    ctx.stroke();
+    if(c){
+        var ctx = c.getContext("2d");
+        ctx.beginPath();
+        ctx.arc(95,50,40,0,2*Math.PI);
+        ctx.stroke();
+    }
 }
+
+init()
+//
+// while(1){
+//
+// }
